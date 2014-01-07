1c60f047125fe50bfb6bfe43beca92891b7f9b5a
梅本金 <meibenjin@gmail.com>
2014-01-07 15:02:42 +0800

    Update gsearch.py

--- gsearch.py
+++ gsearch.py
@@ -19,18 +19,7 @@ from bs4 import BeautifulSoup
 base_url = 'https://www.google.com.hk/'
 results_per_page = 10
 
-user_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0', \
-        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0', \
-        'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533+ \
-        (KHTML, like Gecko) Element Browser 5.0', \
-        'IBM WebExplorer /v0.94', 'Galaxy/1.0 [en] (Mac OS X 10.5.6; U; en)', \
-        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)', \
-        'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14', \
-        'Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) \
-        Version/6.0 Mobile/10A5355d Safari/8536.25', \
-        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) \
-        Chrome/28.0.1468.0 Safari/537.36', \
-        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; TheWorld)']
+user_agents = list()
 
 # results from the search engine
 # basically include url, title,content
@@ -177,8 +166,6 @@ def load_user_agent():
     fp = open('./user_agents', 'r')
 
     line  = fp.readline().strip('\n')
-    global user_agents 
-    user_agents = list()
     while(line):
         user_agents.append(line)
         line = fp.readline()
@@ -192,7 +179,7 @@ def crawler():
     api = GoogleAPI()
 
     # set expect search results to be crawled
-    expect_num = 20
+    expect_num = 10
     # if no parameters, read query keywords from file
     if(len(sys.argv) < 2):
         keywords = open('./keywords', 'r')
